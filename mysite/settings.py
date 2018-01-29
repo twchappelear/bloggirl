eaae2788be029aa699849daad2705e57b54b846c
twchappelear <twchappelear@unomaha.edu>
2018-01-29 08:01:58 -0600

    Blog girl to change '*'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(m=843^3bgc&nes1(3+t^+k&t*j28znd4w2yxenptd1gf@0g04'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
